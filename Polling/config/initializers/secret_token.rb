4314fd3c3c84417241f7d69fca3b3f234e722df1
Simon Lepkin & Andy Yong <Simon+Andy@appacademy.io>
2013-07-04 09:46:01 -0400

    First commit

---
+++ Polling/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polling::Application.config.secret_token = '94a4c5a18eeb82da7bc0e2c0349d260fad855f5a5436a2c37b0002f3aa34979baad3c599c345a04dbcfe3ba8dbdf8be4d68061a488116680abd0749e167ec87b'
